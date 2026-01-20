003ecbf5e3a1eb117a151d93f0f4f098e6ce8c82
gdalevicholga-ux <gdalevicholga@gmail.com>
2026-01-20 20:15:08 +0200

    PositiveTest

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,8 +1,10 @@
 package ui_tests;
 
+import dto.User;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends AppManager {
     @Test
@@ -10,5 +12,20 @@ public class LoginTests extends AppManager {
         System.out.println("first test");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm("family@mail.ru",
+                "Family123!");
+        loginPage.clickBtnLoginForm();
+    }
+
+    @Test
+    public void loginPositiveTestWithUser(){
+        User user = new User("family@mail.ru",
+                "Family123!");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
     }
 }
