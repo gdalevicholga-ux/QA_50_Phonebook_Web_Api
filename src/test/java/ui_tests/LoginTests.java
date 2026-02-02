6de8049d2a8446003706c9fd6e194f014c6e2f08
gdalevicholga-ux <gdalevicholga@gmail.com>
2026-02-02 20:59:44 +0200

    RegistrationNegative Test

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -3,48 +3,111 @@ package ui_tests;
 import dto.User;
 import manager.AppManager;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.ContactPage;
 import pages.HomePage;
 import pages.LoginPage;
 
+import static utils.UserFactory.positiveUser;
+
 public class LoginTests extends AppManager {
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void goToLoginPage() {
+        new HomePage(getDriver()).clickBtnLogin();
+        loginPage = new LoginPage(getDriver());
+    }
     @Test
     public void loginPositiveTest(){
-        System.out.println("first test");
+        // System.out.println("first test");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
         LoginPage loginPage = new LoginPage(getDriver());
-        loginPage.typeLoginRegistrationForm("bolik@gmail.com",
-                "Bolik12345!");
+        loginPage.typeLoginRegistrationForm("family@mail.ru",
+                "Family123!");
         loginPage.clickBtnLoginForm();
-
-        ContactsPage contactsPage = new ContactsPage(getDriver());
-        Assert.assertTrue(contactsPage.isBtnAddDisplayed());
-       // Assert.assertTrue(new ContactsPage(getDriver()).isTextInBtnSignOutPresent("Sign Out"));
+        Assert.assertTrue(new ContactPage(getDriver())
+                .isTextInBtnAddPresent("ADD"));
     }
 
     @Test
     public void loginPositiveTestWithUser(){
-        User user = new User("bolik@gmail.com",
-                "Bolik12345!");
+        User user = new User("family@mail.ru",
+                "Family123!");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnLoginForm();
-
-        ContactsPage contactsPage = new ContactsPage(getDriver());
-        Assert.assertTrue(contactsPage.isBtnContactsDisplayed());
+        Assert.assertTrue(new ContactPage(getDriver()).isTextInBtnSignOutPresent("Sign Out"));
     }
+
     @Test
     public void loginNegativeTest_WrongEmail(){
-        User user = new User("bolikgmail.com","Bolik12345!");
+        User user = new User("familymail.ru", "Family123!");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnLoginForm();
-        Assert.assertEquals(loginPage.closeAlertReturnText(),"Wrong email or password");
-
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
     }
+    @Test
+    public void loginNegativeTest_WrongPassword() {
+        User user = new User("margotest@mail.ru", "WrongPassword123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
+    }
+
+    @Test
+    public void loginNegativeTest_EmptyEmail(){
+        User user = new User("", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
+    }
+
+    @Test
+    public void loginNegativeTest_EmptyPassword(){
+        User user = new User("margotest@mail.ru", "");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
+    }
+
+    @Test
+    public void loginNegativeTest_EmptyFields(){
+        User user = new User("", "");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
+    }
+
+    @Test
+    public void loginNegativeTest_UnregisteredUser(){
+        User user = positiveUser();
+        System.out.println("Trying to login with unregistered user: " + user);
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
+    }
+
+    @Test
+    public void loginNegativeTest_EmailWithoutDomain(){
+        User user = new User("margotest@", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
+    }
+
 }
--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -5,6 +5,7 @@ import manager.AppManager;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.ContactPage;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -25,7 +26,7 @@ public class RegistrationTests extends AppManager {
         User user = new User("masha"+i+"@gmail.com", "Password123!");
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnRegistrationForm();
-        Assert.assertTrue(new ContactsPage(getDriver()).isTextInContactPageMessagePresent("No Contacts here!"));
+        Assert.assertTrue(new ContactPage(getDriver()).isTextInContactPageMessagePresent("No Contacts here!"));
     }
 
     @Test
@@ -34,6 +35,113 @@ public class RegistrationTests extends AppManager {
         System.out.println(user);
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnRegistrationForm();
-        Assert.assertTrue(new ContactsPage(getDriver()).isTextInContactPageMessagePresent("No Contacts here!"));
+        Assert.assertTrue(new ContactPage(getDriver()).isTextInContactPageMessagePresent("No Contacts here!"));
+    }
+    @Test
+    public void registrationNegativeTest_WrongEmailFormat(){
+        User user = new User("wrongemailformat", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_EmptyEmail(){
+        User user = new User("", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_EmptyPassword(){
+        User user = new User(positiveUser().getUsername(), "");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_EmptyFields(){
+        User user = new User("", "");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_PasswordWithoutUpperCase(){
+        User user = new User(positiveUser().getUsername(), "password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_PasswordWithoutLowerCase(){
+        User user = new User(positiveUser().getUsername(), "PASSWORD123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_PasswordWithoutDigit(){
+        User user = new User(positiveUser().getUsername(), "Password!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_PasswordWithoutSpecialChar(){
+        User user = new User(positiveUser().getUsername(), "Password123");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_ShortPassword(){
+        User user = new User(positiveUser().getUsername(), "Pass1!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_EmailWithoutAtSymbol(){
+        User user = new User("margotestgmail.com", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_EmailWithoutDomain(){
+        User user = new User("margotest@", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password format");
+    }
+
+    @Test
+    public void registrationNegativeTest_DuplicateUser(){
+        User user = new User("margotest@gmail.com", "Password123!");
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnRegistrationForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "User already exist");
     }
 }
